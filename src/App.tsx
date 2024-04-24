affbd3a6a6eaaaae8268590fadee467b0084fe17
Garfield <demyanovaya@gmail.com>
2024-04-24 21:52:32 +0200

    Tipisaziya

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+//Lessons
+import Lessons_06 from './lessons/Lessons_06/Lessons_06';
+
+//Homeworks
+//Consultations
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<Lessons_06 />
+{/* <Homework_06/> */}
+
     </div>
   );
 }
